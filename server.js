0b5ca283bf73dea22ef80e765bca24a8690f8c3f
sibtain_usa <superali_usa@yahoo.com>
2016-03-09 20:57:28 -0500

    initial commit changes to server

--- server.js
+++ server.js
@@ -1,35 +1,66 @@
 var express = require('express'),
     logger  = require('morgan')('dev'),
     path    = require('path'),
+    mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    bodyParser = require('body-parser'),
     server  = express();
 
+//Todo Model
+var todoSchema = new Schema({
+desc: {
+type: String,
+required: true
+},
+completed: {
+  type: Boolean,
+  required: true
+}
+});
 
+var Todo = mongoose.model('Todo', todoSchema);
+
+//create a connection to our db
+mongoose.connect('mongodb://localhost/todoApplication')
 var port = process.env.PORT || 9000;
 
 server.use(express.static(path.join(__dirname,'public')));
 server.use(logger);
 
-
+server.use(bodyParser.json()); //for parsing application/json
+server.use(bodyParser.urlencoded({extended: true}));
 
 server.get('/', function(req, res){
   res.send('this is a starter application, welcome!');
 });
 
-server.get('/json', function(req, res){
-  var someJson = [
-    {
-      desc: 'learn how to build a web based api',
-      completed: false
-    },
-    {
-      desc: 'build an Angular frontend that consumes the api',
-      completed: false
-    }
-  ];
+server.get('/api/todos', function(req, res){
+  Todo.find(function(err, todos){
+    if(err) throw err;
 
-  res.json(someJson);
+    res.json(todo);
+  });
 });
 
+server.post('/api/todos', function(req, res){
+  var desc =req.body.desc;
+  var completed = req.body.completed;
+  var todoObj = {
+    desc: desc,
+    completed: false
+  };
+  Todo.create(todoObj, function(err, todo){
+
+  });
+});
+
+server.put('/api/todos/:id', function(req, res){
+res.send('I updated a todo!');
+});
+
+server.delete('/api/todos/:id', function(req, res){
+res.send('Oh no! I deleted todo!');
+});
 server.listen(port, function(){
   console.log('Now listening on port ' + port);
 });
